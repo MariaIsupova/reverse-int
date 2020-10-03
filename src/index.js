94c3afc6280b3e7f47de8ced81bb8cb27c5d312f
MariaIsupova <Mariaisisupova@gmail.com>
2020-10-03 15:13:17 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let res = ''
+    if(n<0) n = n.toString().slice(1)
+    else n = n.toString()
+    
+    for (i = 0; i < n.length; i += 1) res = n[i] + res
+    return res
 }
